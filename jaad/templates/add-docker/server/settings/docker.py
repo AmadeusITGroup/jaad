82e06dee8d85cfde5bf2ccc11359c7b0470710ac
tools4origins <tools4origins@gmail.com>
2022-04-08 18:23:12 +0200

    feat(docker): Simplify docker integration

---
+++ jaad/templates/add-docker/server/settings/docker.py
@@ -0,0 +1,7 @@
+# noinspection PyUnresolvedReferences
+# This must be import first as it overrides Python path
+from .application import *  # noqa F401,F403
+
+SECRET_KEY = "This secret key is not production ready"
+
+DEBUG = True
